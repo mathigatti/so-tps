59236514ac9f4e043985b11a822a3fa8a20c3c99
mathias gatti <mathigatti@gmail.com>
2017-05-17 23:46:45 -0300

    Agrego tests automaticos

--- TPs/TP2/test-1.cpp
+++ TPs/TP2/test-1.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include "ListaAtomica.hpp"
+#include <assert.h>     /* assert */
 
 using namespace std;
 
 int main(void) {
 	Lista<int> listaAtomica;
 	listaAtomica.push_front(1);
+
+	printf("TESTEANDO...\n");
+	assert(listaAtomica.front() == 1);
 	listaAtomica.push_front(2);
+	assert(listaAtomica.front() == 2);
 	listaAtomica.push_front(3);
-	printf("El numero al frente es %d\n",listaAtomica.front());
+	assert(listaAtomica.front() == 3);
+	printf("TODOS LOS TESTS PASARON EXITOSAMENTE\n");
 
 	return 0;
 }
